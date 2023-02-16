7c4a83fbe279cbca0e505a699632b35c7bb19511
henritresor <shimwamanahenritresor@gmail.com>
2023-02-16 17:46:19 +0200

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 int main(void)
 {
 /**
- * main - Entry point
- *
  * This function is the entry point of the program. It prints a message to
  * the standard output using the `puts` function and returns 0 to indicate
  * success.
  *
  * Return: Always 0 (Success)
-*/
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
