ef7722063bccb9fe9cfdb492e8e5368fbbb3f700
henritresor <shimwamanahenritresor@gmail.com>
2023-02-16 18:08:05 +0200

    added a new file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char %d byte(s)",sizeof(char));
+printf("Size of an int %d byte(s)",sizeof(int));
+printf("Size of a long int %ld byte(s)",sizeof(long int));
+printf("Size of a long long int %ld byte(s)",sizeof(long long int));
+printf("Size of a float %ld byte(s)",sizeof(float));
+return (0);
+}
