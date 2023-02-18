240fe164ef4fe41e502bed90030006347b7dccb3
henritresor <shimwamanahenritresor@gmail.com>
2023-02-18 05:16:12 +0200

    first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ * 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+if(n>0)
+{
+printf("%d is positive", n);
+}
+else if(n<0)
+{
+printf("%d is negative", n);
+}
+else
+{
+printf("%d is zero", n);
+}
+return (0);
+}
