c8a42c9b3d7e23516bd8cbbb9fb0fdf5e1c374c0
henritresor <shimwamanahenritresor@gmail.com>
2023-02-18 05:48:46 +0200

    added new files

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return:0 (success)
+ */
+int main(void)
+{
+putchar('h');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+putchar('f');
+return (0);
+}
