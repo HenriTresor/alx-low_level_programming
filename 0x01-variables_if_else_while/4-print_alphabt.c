a48d982f78af6e9b7432994c008482807c898eb8
henritresor <shimwamanahenritresor@gmail.com>
2023-02-18 05:34:57 +0200

    r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet;
+if(alphabet!=q || alphabet!=e)
+{
+putchar(strlwr(alphabet));
+}
+return (0);
+}
