14dbbbb66472fb265437c353b0e7f6523a4e2b06
henritresor <shimwamanahenritresor@gmail.com>
2023-02-20 20:53:32 +0200

    added n in printf function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
 {
-printf("%d is positive\n");
+printf("%d is positive\n", n);
 }
 else if(n<0)
 {
-printf("%d is negative\n");
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero");
+printf("%d is zero", n);
 }
 return (0);
 }
