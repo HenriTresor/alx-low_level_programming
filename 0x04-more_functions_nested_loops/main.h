42f6156d63673122e33dc4610d19ebb6934663a7
henritresor <shimwamanahenritresor@gmail.com>
2023-02-23 21:21:53 +0200

    modified main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,40 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+/* 0-isupper.c */
 int _isupper(int c);
+
+/* 1-isdigit.c */
 int _isdigit(int c);
+
+/* 2-mul.c */
 int mul(int a, int b);
+
+/* 3-print_numbers.c */
+void print_numbers(void);
+
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* 5-more_numbers.c */
+void more_numbers(void);
+
+/* 6-print_line.c */
+void print_line(int n);
+
+/* 7-print_diagonal.c */
+void print_diagonal(int n);
+
+/* 8-print_square.c */
+void print_square(int size);
+
+/* 9-fizz_buzz.c */
+void fizz_buzz(void);
+
+/* 10-print_triangle.c */
+void print_triangle(int size);
+
+/* _putchar.c */
+int _putchar(char c);
 
 #endif /* MAIN_H */
