d73e8f824321a7395ad55feac57850a4c1194739
henritresor <shimwamanahenritresor@gmail.com>
2023-02-28 17:46:11 +0200

    initialised project

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /*
- *_strlen - returns the length of a string
- *@s: pointer to the first character of the string
+ * _strlen - returns the length of a string
+ * @s: pointer to the first character of the string
  *
- *Return: the length of the string
+ * Return: the length of the string
  */
 
 int _strlen(char *s)
 {
 int length;
-length=0;
+length = 0;
 while (*s != '\0')
 {
 length++;
 s++;
 }
-return length;
+return (length);
 }
