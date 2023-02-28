0f4a2496147cd93c1c5c4a846e152a65e86e1333
henritresor <shimwamanahenritresor@gmail.com>
2023-02-28 19:33:40 +0200

    working on arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+
 /**
  * _strcpy - copies the string pointed to by src, including
  * the terminating null byte (\0), to the buffer pointed to by dest.
@@ -15,5 +16,5 @@ for (i = 0; src[i] != '\0'; i++)
 dest[i] = src[i];
 }
 dest[i] = '\0';
-return dest;
+return (dest);
 }
