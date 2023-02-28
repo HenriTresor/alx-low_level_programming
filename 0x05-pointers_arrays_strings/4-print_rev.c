9186858c5f2a00128b84321cea3aff653de51417
henritresor <shimwamanahenritresor@gmail.com>
2023-02-28 18:01:04 +0200

    some changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,16 @@
  */
 void print_rev(char *s)
 {
-    int i = 0;
-    int len = 0;
-
-    while (s[i] != '\0')
-    {
-        len++;
-        i++;
-    }
-
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-
-    _putchar('\n');
+int i = 0;
+int len = 0;
+while (s[i] != '\0')
+ {
+len++;
+i++;
+}
+for (i = len - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
 }
