14281705f1f527a2f993520a93f51f8b4fdde964
henritresor <shimwamanahenritresor@gmail.com>
2023-02-28 19:39:40 +0200

    working on arrays

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char *s)
+{
+int sign = 1, result = 0, i = 0;
+while (s[i] != '\0') {
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+result = (result * 10) + (s[i] - '0');
+else if (result > 0)
+break;
+i++;
+}
+return (sign * result);
+}
